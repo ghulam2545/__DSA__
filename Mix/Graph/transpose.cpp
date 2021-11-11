6467b6239a16935f5e7fc4b277ad6ba6288d7709
ghulam2545 <ghulam2545@gmail.com>
2021-11-11 22:09:12 +0530

    message

--- Mix/Graph/transpose.cpp
+++ Mix/Graph/transpose.cpp
@@ -1,23 +1,20 @@
-// little messed up, has to clean
-// but working fine
-
 #include <iostream>
 #include <utility>
 #include <vector>
 using namespace std;
-// class adjMatrix {
-//    public:
-//     int** create(int n, int m);
-//     void addEdge(int** g, int src, int des, int wt);
-//     void print(int** g);
-//     int** transpose(int** g);
-//     ~adjMatrix();
+class adjMatrix {
+   public:
+    int** create(int n, int m);
+    void addEdge(int** g, int src, int des, int wt);
+    void print(int** g);
+    int** transpose(int** g);
+    ~adjMatrix();
 
-//    private:
-//     int vertex;
-//     int edge;
-//     int** arr;
-// };
+   private:
+    int vertex;
+    int edge;
+    int** arr;
+};
 class adjList {
    public:
     vector<pair<int, int>>* create(int n, int m);
@@ -32,18 +29,19 @@ class adjList {
     vector<pair<int, int>>* v;
 };
 int main() {
-    // int a, b;
-    // cin >> a >> b;
-    // adjMatrix* oo = new adjMatrix();
-    // int** gr = oo->create(a, b);
-    // int p, q, r;
-    // for (int i = 0; i < b; ++i) {
-    //     cin >> p >> q >> r;
-    //     oo->addEdge(gr, p, q, r);
-    // }
-    // oo->print(gr);
-    // cout << "\n\n";
-    // oo->print(oo->transpose(gr));
+    /*
+    int a, b;
+    cin >> a >> b;
+    adjMatrix* oo = new adjMatrix();
+    int** gr = oo->create(a, b);
+    int p, q, r;
+    for (int i = 0; i < b; ++i) {
+        cin >> p >> q >> r;
+        oo->addEdge(gr, p, q, r);
+    }
+    oo->print(gr);
+    cout << "\n\n";
+    oo->print(oo->transpose(gr));
 
     adjList* oo = new adjList();
     int a, b;
@@ -58,50 +56,51 @@ int main() {
     oo->print(gr);
     cout << "\n";
     oo->print(oo->transpose(gr));
+    */
     return 0;
 }
-// int** adjMatrix::create(int n, int m) {
-//     vertex = n;
-//     edge = m;
-//     arr = new int*[vertex];
-//     for (int i = 0; i < vertex; ++i) {
-//         arr[i] = new int[vertex];
-//     }
-//     for (int i = 0; i < vertex; ++i) {
-//         for (int j = 0; j < vertex; ++j) {
-//             arr[i][j] = 0;
-//         }
-//     }
-//     return arr;
-// }
-// void adjMatrix::addEdge(int** g, int src, int des, int wt) {
-//     //
-//     g[src][des] = wt;
-// }
-// void adjMatrix::print(int** g) {
-//     for (int i = 0; i < vertex; ++i) {
-//         for (int j = 0; j < vertex; ++j) {
-//             cout << g[i][j] << " ";
-//         }
-//         cout << "\n";
-//     }
-// }
-// int** adjMatrix::transpose(int** g) {
-//     int** tmat;
-//     tmat = create(vertex, vertex);
-//     for (int i = 0; i < vertex; ++i) {
-//         for (int j = 0; j < vertex; ++j) {
-//             tmat[i][j] = g[j][i];
-//         }
-//     }
-//     return tmat;
-// }
-// adjMatrix::~adjMatrix() {
-//     for (int i = 0; i < vertex; ++i) {
-//         delete[] arr[i];
-//     }
-//     delete[] arr;
-// }
+int** adjMatrix::create(int n, int m) {
+    vertex = n;
+    edge = m;
+    arr = new int*[vertex];
+    for (int i = 0; i < vertex; ++i) {
+        arr[i] = new int[vertex];
+    }
+    for (int i = 0; i < vertex; ++i) {
+        for (int j = 0; j < vertex; ++j) {
+            arr[i][j] = 0;
+        }
+    }
+    return arr;
+}
+void adjMatrix::addEdge(int** g, int src, int des, int wt) {
+    //
+    g[src][des] = wt;
+}
+void adjMatrix::print(int** g) {
+    for (int i = 0; i < vertex; ++i) {
+        for (int j = 0; j < vertex; ++j) {
+            cout << g[i][j] << " ";
+        }
+        cout << "\n";
+    }
+}
+int** adjMatrix::transpose(int** g) {
+    int** tmat;
+    tmat = create(vertex, vertex);
+    for (int i = 0; i < vertex; ++i) {
+        for (int j = 0; j < vertex; ++j) {
+            tmat[i][j] = g[j][i];
+        }
+    }
+    return tmat;
+}
+adjMatrix::~adjMatrix() {
+    for (int i = 0; i < vertex; ++i) {
+        delete[] arr[i];
+    }
+    delete[] arr;
+}
 vector<pair<int, int>>* adjList::create(int n, int m) {
     vertex = n;
     edge = m;
